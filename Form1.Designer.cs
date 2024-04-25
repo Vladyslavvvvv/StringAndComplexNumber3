4c6d5e90cb574455024aee482c9a395ee682ee64
Vlad <matviykiv.v@nltu.lviv.ua>
2024-04-26 01:06:41 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace StringAndComplexNumber3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxFirst = new System.Windows.Forms.TextBox();
+            this.textBoxSecond = new System.Windows.Forms.TextBox();
+            this.labelEquals = new System.Windows.Forms.Label();
+            this.labelAddition = new System.Windows.Forms.Label();
+            this.labelMultiplication = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.AutoSize = true;
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(640, 60);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(77, 39);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(10, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "First complex number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(10, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(302, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Second complex number";
+            // 
+            // textBoxFirst
+            // 
+            this.textBoxFirst.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxFirst.Location = new System.Drawing.Point(330, 10);
+            this.textBoxFirst.Name = "textBoxFirst";
+            this.textBoxFirst.Size = new System.Drawing.Size(200, 34);
+            this.textBoxFirst.TabIndex = 3;
+            // 
+            // textBoxSecond
+            // 
+            this.textBoxSecond.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSecond.Location = new System.Drawing.Point(330, 110);
+            this.textBoxSecond.Name = "textBoxSecond";
+            this.textBoxSecond.Size = new System.Drawing.Size(200, 34);
+            this.textBoxSecond.TabIndex = 4;
+            // 
+            // labelEquals
+            // 
+            this.labelEquals.AutoSize = true;
+            this.labelEquals.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelEquals.Location = new System.Drawing.Point(10, 210);
+            this.labelEquals.Name = "labelEquals";
+            this.labelEquals.Size = new System.Drawing.Size(106, 29);
+            this.labelEquals.TabIndex = 5;
+            this.labelEquals.Text = "Equals?";
+            // 
+            // labelAddition
+            // 
+            this.labelAddition.AutoSize = true;
+            this.labelAddition.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelAddition.Location = new System.Drawing.Point(10, 310);
+            this.labelAddition.Name = "labelAddition";
+            this.labelAddition.Size = new System.Drawing.Size(109, 29);
+            this.labelAddition.TabIndex = 6;
+            this.labelAddition.Text = "Addition";
+            // 
+            // labelMultiplication
+            // 
+            this.labelMultiplication.AutoSize = true;
+            this.labelMultiplication.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelMultiplication.Location = new System.Drawing.Point(10, 410);
+            this.labelMultiplication.Name = "labelMultiplication";
+            this.labelMultiplication.Size = new System.Drawing.Size(168, 29);
+            this.labelMultiplication.TabIndex = 7;
+            this.labelMultiplication.Text = "Multiplication";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(300, 260);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(482, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Examples of a complex number: 4 + 5i, -1 + -8i, -9 + -1i.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(300, 230);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(462, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "For example, the numbers 4 + -5i and 4+-5i are equal.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(300, 200);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(718, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "The program may not compare numbers correctly due to different number of spaces.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1022, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelMultiplication);
+            this.Controls.Add(this.labelAddition);
+            this.Controls.Add(this.labelEquals);
+            this.Controls.Add(this.textBoxSecond);
+            this.Controls.Add(this.textBoxFirst);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxFirst;
+        private System.Windows.Forms.TextBox textBoxSecond;
+        private System.Windows.Forms.Label labelEquals;
+        private System.Windows.Forms.Label labelAddition;
+        private System.Windows.Forms.Label labelMultiplication;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringAndComplexNumber3
+{
+    // Custom interface inheriting from the base class
+    public interface IString
+    {
+        byte Length();
+        void Clear();
+    }
+
+    // IComparable and IClonable interfaces are implemented in the base class tring
+    // Class representing the main form
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Method to check the validity of a complex number input
+        private bool IsValidComplexNumber(string input)
+        {
+            // Divide the line by the "+" sign
+            string[] parts = input.Split('+');
+
+            // If after splitting, the string consists of two parts
+            if (parts.Length == 2)
+            {
+                // Checking the first part for numbers
+                if (parts[0].Any(char.IsDigit))
+                {
+                    // Checking the second part for numbers and the "i" symbol at the end
+                    if (parts[1].TrimEnd().Any(char.IsDigit) && parts[1].TrimEnd().EndsWith("i"))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        // Event handler for the button click
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            string firstInput = textBoxFirst.Text;
+            string secondInput = textBoxSecond.Text;
+
+            // Checking the correctness of entering the first number
+            if (!IsValidComplexNumber(firstInput))
+            {
+                MessageBox.Show("Incorrect input format for first number!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Checking the correctness of entering the second number
+            if (!IsValidComplexNumber(secondInput))
+            {
+                MessageBox.Show("Incorrect input format for second number!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Create complex numbers based on the entered lines
+            ComplexNumber firstComplex = new ComplexNumber(firstInput);
+            ComplexNumber secondComplex = new ComplexNumber(secondInput);
+
+            // Check for equality
+            bool isEqual = firstComplex.Equals(secondComplex);
+            labelEquals.Text = "Equals? " + isEqual.ToString();
+
+            // Adding numbers
+            ComplexNumber sum = firstComplex.Add(secondComplex);
+            labelAddition.Text = "Addition: " + sum.ToString();
+
+            // Multiplication of numbers
+            ComplexNumber product = firstComplex.Multiply(secondComplex);
+            labelMultiplication.Text = "Multiplication: " + product.ToString();
+
+            // Creating an array of objects
+            ComplexNumber[] numbers = new ComplexNumber[4];
+
+            // Filling the first half of the array
+            numbers[0] = firstComplex;
+            numbers[1] = secondComplex;
+
+            // Filling the second half of the array with clones of the first half
+            for (int i = 2; i < 4; i++)
+            {
+                numbers[i] = (ComplexNumber)numbers[i - 2].Clone();
+            }
+
+            try
+            {
+                // Sorting the array
+                Array.Sort(numbers);
+
+                // Displaying the sorted array using MessageBox
+                string sortedNumbers = "Sorted Complex Numbers:\n";
+                foreach (ComplexNumber num in numbers)
+                {
+                    sortedNumbers += num.ToString() + "\n";
+                }
+                MessageBox.Show(sortedNumbers, "Sorted Complex Numbers", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+
+    // Base class
+    public abstract class tring : IComparable, ICloneable, IString
+    {
+        protected string value;
+        protected byte length;
+
+        public tring()
+        {
+            value = "";
+            length = 0;
+        }
+
+        public tring(string str)
+        {
+            value = str;
+            length = (byte)str.Length;
+        }
+
+        public tring(char ch)
+        {
+            value = ch.ToString();
+            length = 1;
+        }
+
+        // Method to return the length of the string
+        public byte Length()
+        {
+            return length;
+        }
+
+        // Abstract method to clear the string
+        public abstract void Clear();
+
+        // Method to compare objects
+        public int CompareTo(object obj)
+        {
+            tring other = obj as tring;
+            if (other == null)
+            {
+                MessageBox.Show("Object is not a tring");
+            }
+            return this.length.CompareTo(other.length);
+        }
+
+        // Method to clone an object
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+        // Method to return the string representation of the object
+        public override string ToString()
+        {
+            return value;
+        }
+    }
+
+    // Class representing a complex number, inheriting from tring and implementing IComparable and ICloneable
+    public class ComplexNumber : tring, IComparable, ICloneable
+    {
+        public ComplexNumber() : base()
+        {
+        }
+
+        public ComplexNumber(string str) : base(str)
+        {
+        }
+
+        // Method to clear the complex number
+        public override void Clear()
+        {
+            value = "";
+            length = 0;
+        }
+
+        // Method to check if two complex numbers are equal
+        public bool Equals(ComplexNumber other)
+        {
+            return this.value.Equals(other.value);
+        }
+
+        // Method to add two complex numbers
+        public ComplexNumber Add(ComplexNumber other)
+        {
+            double realPart1 = ExtractRealPart(this.value);
+            double imagPart1 = ExtractImaginaryPart(this.value);
+            double realPart2 = ExtractRealPart(other.value);
+            double imagPart2 = ExtractImaginaryPart(other.value);
+
+            double resultReal = realPart1 + realPart2;
+            double resultImag = imagPart1 + imagPart2;
+
+            return new ComplexNumber(resultReal.ToString() + "+" + resultImag.ToString() + "i");
+        }
+
+        // Method to multiply two complex numbers
+        public ComplexNumber Multiply(ComplexNumber other)
+        {
+            double realPart1 = ExtractRealPart(this.value);
+            double imagPart1 = ExtractImaginaryPart(this.value);
+            double realPart2 = ExtractRealPart(other.value);
+            double imagPart2 = ExtractImaginaryPart(other.value);
+
+            double resultReal = (realPart1 * realPart2) - (imagPart1 * imagPart2);
+            double resultImag = (realPart1 * imagPart2) + (imagPart1 * realPart2);
+
+            return new ComplexNumber(resultReal.ToString() + "+" + resultImag.ToString() + "i");
+        }
+
+        // Method to extract the real part of a complex number
+        private double ExtractRealPart(string complexNumber)
+        {
+            string[] parts = complexNumber.Split('+');
+            return double.Parse(parts[0]);
+        }
+
+        // Method to extract the imaginary part of a complex number
+        private double ExtractImaginaryPart(string complexNumber)
+        {
+            string[] parts = complexNumber.Split('+');
+            string imagPart = parts[1].Substring(0, parts[1].Length - 1);
+            return double.Parse(imagPart);
+        }
+
+        // Method to return the string representation of the object
+        public override string ToString()
+        {
+            return value;
+        }
+
+        // Method to compare objects
+        public int CompareTo(object obj)
+        {
+            if (obj == null)
+                return 1;
+
+            ComplexNumber other = obj as ComplexNumber;
+            if (other != null)
+            {
+                // Comparing complex numbers based on their values
+                double thisRealPart = ExtractRealPart(this.value);
+                double thisImagPart = ExtractImaginaryPart(this.value);
+                double otherRealPart = ExtractRealPart(other.value);
+                double otherImagPart = ExtractImaginaryPart(other.value);
+
+                // Comparing real parts first
+                int realComparison = thisRealPart.CompareTo(otherRealPart);
+                if (realComparison != 0)
+                    return realComparison;
+
+                // If real parts are equal, comparing imaginary parts
+                return thisImagPart.CompareTo(otherImagPart);
+            }
+            else
+            {
+                MessageBox.Show("Object is not a ComplexNumber");
+                // If the type is not ComplexNumber, we cannot determine the sorting order,
+                // so it's better to throw an exception rather than just displaying a message
+                throw new ArgumentException("Object is not a ComplexNumber");
+            }
+        }
+
+        // Method to clone an object
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
